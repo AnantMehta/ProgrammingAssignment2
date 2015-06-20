1a8db032752597f1ae71860cbe978768f7ab331c
AnantMehta <anant.i.mehta@gmail.com>
2015-06-20 19:28:28 +0530

    Assignment 2
    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Returns the list of available functions(for eg. get,getinverse etc) within the function.
+#Takes Matrix as an input.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x =matrix(nrow=0, ncol=0))
+{
+	m <- NULL
+	set <- function(y)
+		{
+			x <<- y
+			m <<-NULL
+		}
+	get <- function() x
 
+	setinverse <- function(inv) 
+	{
+		m <<- inv
+		
+	}
+	getinverse <- function() m
+
+	list(get = get , set = set , getinverse = getinverse , setinverse = setinverse)
 }
 
+#Gets like actual matrix and inverse of the matrix from the cache.
+#if input matrix and the matrix from the cache are equal and the inverse of the matrix is present in cache.
+#Then return the the cached value
+#Else compute the inverse and replace matrix and inverse in the cahce.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,mat) 
+{
+	m <- x$getinverse()	
+	cachedmat <- x$get()
+	 
+ 	if(length(m) != 0 && all(dim(cachedmat) == dim(mat)) && all(cachedmat == mat))
+		{
+			print("Returning Cached Value")
+			return(m)
+		}
+	else
+		{
+			x$set(mat)
+			m <- solve(mat)
+			x$setinverse(m)
+			m
+		}
 }
